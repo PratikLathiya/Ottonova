407bd4beb9e39a8f38f980c353b7faa4e0648075
PratikLathiya <pratiklathiya39@gmail.com>
2023-07-11 09:38:25 +0200

    add gender button css

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 export default function Userform() {
-    // const [imageSrc, setImageSrc] = useState('./man1.png');
-    // const [imageSrc1, setImageSrc1] = useState('./female.png');
-    // const [isGenderActive, setGenderActive] = useState(false);
-    // const [isGenderActive1, setGenderActive1] = useState(false);
-
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [gender, setGender] = useState(null);
+
+    const formRef = useRef(null);
 
     const handleNameChange = (e) => {
         setName(e.target.value);
@@ -29,49 +26,22 @@ export default function Userform() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!gender) {
+            alert('Please select a gender');
+            return;
+        }
         // Handle form submission
+
+        // Reset form
+        formRef.current.reset();
+        setGender(null);
     };
 
-    // let mystyle = {
-    //     margin: "5px",
-    //     border: "0.5px solid #c7c2c2",
-    //     backgroundColor: imageSrc === './man1.png' ? "white" : "rgb(218 225 231)"
-    // }
-    // let mystyle1 = {
-    //     border: "0.5px solid #c7c2c2",
-    //     backgroundColor: imageSrc1 === './female.png' ? "white" : "#e9dde1"
-    // }
-
-    // const changeImageSrc = () => {
-    //     if (imageSrc === './man1.png') {
-    //         setImageSrc('./activemale.png');
-    //         setImageSrc1('./female.png');
-    //         setGenderActive(!isGenderActive);
-    //     } else {
-    //         setImageSrc('./man1.png');
-    //         // setImageSrc1('./activefemale.png')
-    //     }
-    // }
-
-    // const changeImageSrc1 = () => {
-    //     if (imageSrc1 === './female.png') {
-    //         setImageSrc1('./activefemale.png');
-    //         setImageSrc('./man1.png');
-    //         setGenderActive1(!isGenderActive);
-    //     } else {
-    //         setImageSrc1('./female.png');
-    //         // setImageSrc('./activemale.png')
-    //     }
-    // }
     return (
         <>
-            {/* <div className="ocean">
-                <div className="wave"></div>
-                <div className="wave"></div>
-            </div> */}
             <div className="my-4 container ocean">
                 <div className="container card center shadow p-3 mb-5 bg-body rounded cardstyle">
-                    <form className="g-3 needs-validation" onSubmit={handleSubmit}>
+                    <form className="g-3 needs-validation" ref={formRef} onSubmit={handleSubmit}>
                         <div className="my-3">
                             <label htmlFor="validationCustom01" className="form-label">First name</label>
                             <input type="text" className="form-control" value={name} id="name" onChange={handleNameChange} required />
@@ -115,21 +85,30 @@ export default function Userform() {
                         </div> */}
                         <div className="gender-switch">
                             <span>Gender:</span>
-                            <button
-                                className={`gender-button ${gender === 'male' ? 'active' : ''}`}
-                                onClick={() => handleGenderSelection('male')}
-                            >
-                                <div className="wave-animation"></div>
-                                <img src={gender === 'male' ? 'activemale.png' : 'male.png'} alt="Male" />
-                            </button>
+                            <div className="toggle-button-cover">
+                                <div className="button-cover">
+                                    <div className="button b2" id="button-10">
+                                        <div className="knobs">
+                                            <button
+                                                className={`gender-button ${gender === 'male' ? 'active' : ''}`}
+                                                onClick={() => handleGenderSelection('male')}
+                                            >
+                                                <div className="wave-animation"></div>
+                                                <img src={gender === 'male' ? 'activemale.png' : 'male.png'} alt="Male" />
+                                            </button>
 
-                            <button
-                                className={`gender-button ${gender === 'female' ? 'active' : ''}`}
-                                onClick={() => handleGenderSelection('female')}
-                            >
-                                <div className="wave-animation"></div>
-                                <img src={gender === 'female' ? 'activefemale.png' : 'female.png'} alt="Female" />
-                            </button>
+                                            <button
+                                                className={`gender-button ${gender === 'female' ? 'active' : ''}`}
+                                                onClick={() => handleGenderSelection('female')}
+                                            >
+                                                <div className="wave-animation"></div>
+                                                <img src={gender === 'female' ? 'activefemale.png' : 'female.png'} alt="Female" />
+                                            </button>
+                                        </div>
+                                        <div className="layer"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-12 my-3">
                             <button className="btn btn-primary" type="submit">Submit</button>
